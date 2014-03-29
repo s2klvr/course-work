5a234e1612b43a96e72369d2ee4a824f79fab88b
s2klvr <s2klvr@gmail.com>
2014-03-28 22:35:24 -0700

    Coding Challenges

---
+++ Coding Challenges/ArithmaticProgression/ArithmaticProgression/Program.cs
@@ -0,0 +1,56 @@
+﻿/*
+Programmer: Arron Thornton
+Description:  This is my derived solution to one of the many coding
+challenges.
+The task is, given a set of numbers in arithmatic progression, find and
+output the missing variable
+For example, given the AP of 1, 3, 5, 9, 11, the program would find that 7 is
+missing from the progression and will output 7.
+*/
+using System;
+
+namespace ArithmaticProgression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            int variation = 0;
+            int missingVariable = 0;
+
+
+            Console.WriteLine("Enter length of AP:");
+            n = int.Parse(Console.ReadLine());
+
+            int[] numArray = new int[n];
+
+            for (int i = 0; i < numArray.Length; i++)
+            {
+                Console.Write("\nInteger {0}:\t", i + 1);
+                numArray[i] = int.Parse(Console.ReadLine());
+
+            }
+            Array.Sort(numArray);
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                variation += numArray[i + 1] - numArray[i];
+            }
+
+            variation /= n;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                if (numArray[i + 1] - numArray[i] > variation)
+                {
+                    missingVariable = numArray[i] + variation;
+                }
+            }
+
+            Console.WriteLine("Variation:  {0}", variation);
+            Console.WriteLine("Missing Variable:  {0}", missingVariable);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Coding Challenges/Readme.txt
@@ -0,0 +1,11 @@
+This folder is dedicated to my solutions regarding various coding challenges/interview questions
+that are posted around the internet.
+
+I do not copy and use code belonging to others.  The code you see is the product of my time and
+hard work debugging and troubleshooting as I go along.
+
+
+
+Thank you for looking!
+
+Arron Thornton
